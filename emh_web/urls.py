aa51123ab47f649dd80c9ee859259589aac42293
marqquees <dom.marques@outlook.pt>
2026-06-19 11:55:50 +0100

    feat: update URL patterns for course detail and add student registration endpoint

--- emh_web/urls.py
+++ emh_web/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('course/<int:course_id>', views.course_detail, name='course_detail'),
-    path('enroll/', views.enroll_course, name='enroll_course'),
-    path('portal/student/login/', views.portal_student_login, name='portal_student_login'),
+    path('course/<int:course_id>/', views.course_detail, name='course_detail'),
+    path('student_registration/', views.student_registration, name='student_registration'),
 ]
